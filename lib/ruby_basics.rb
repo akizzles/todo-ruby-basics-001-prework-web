ad8b3429b9c7de9849ee8dafc9d4054c2fc748e3
akizzles <aykim002@gmail.com>
2016-02-07 17:25:17 -0500

    ruby basics of methods wrapup

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
-def division(num1, num2)
- 
+def division(num1=1, num2=1)
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(args="args")
+  args
 end
 
-def greeting
-
+def greeting(name, age)
+  puts "Hello #{name}, you are #{age} yrs old."
 end
 
-def return_a_value
-
+def return_a_value(phrase="Nice")
+  phrase
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+  topping
 end
